783e3fc7f221835aa4ff978b1ee6fcfdae8a1689
fannym-endro <fannym@endro-cosmetiques.com>
2026-08-25 12:16:53 +0200

    Delete endro-dashboard 5 directory

--- endro-dashboard 5/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Endro Dashboard" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body style={{ margin: 0, background: "#fafafa" }}>{children}</body>
-    </html>
-  );
-}
